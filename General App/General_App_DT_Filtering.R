bafc1857caf0c58d801765a032f45daa08ed63b5
Constantin Kumaus <constantin.kumaus@meduniwien.ac.at>
2021-08-02 17:08:17 +0200

    Filtering and default value choice
    continued with the DT filtering and implemented the choice of default values within the DT

--- General App/General_App_DT_Filtering.R
+++ General App/General_App_DT_Filtering.R
@@ -23,17 +23,18 @@ ui <- fluidPage(title = "Simulation Results", # title in browser window tab
                     conditionalPanel("input.exampleData == 0",
                                      radioButtons("datatype", 
                                                   "Which datatype do you upload", 
-                                                  choices = c("Excel", "Csv")),
+                                                  choices = c("Csv"
+                                                              # , "Excel"
+                                                  )),
                                      conditionalPanel("input.datatype == 'Csv'",
                                                       radioButtons("sep", "Csv-Separator", choices = c(",", ";", "\t"))),
-                                     fileInput("file", "Choose file to upload"),
-                                     checkboxInput("change", "Do you want to specify numeric columns?"),
-                                     conditionalPanel("input.change",
-                                                      checkboxGroupInput("numerics", "Choose numeric Variables", choices = NULL),
-                                                      actionButton("go", "Process changes to numeric"))
-                    )
+                                     fileInput("file", "Choose file to upload") 
+                                     # checkboxInput("inputend", "Define last input variable"),
+                                     # conditionalPanel("input.change",
+                                     
+                    ),
                     
-                    
+                    selectInput("inputend", "State last input variable", choices = NULL)
                     
                     
                   ),
@@ -46,18 +47,18 @@ ui <- fluidPage(title = "Simulation Results", # title in browser window tab
                                
                                DT::dataTableOutput("dataDT")), 
                       
-                      tabPanel("Filter variables",
-                               
-                               #adds every checked variable from input$varname as an input element 
-                               # with the corresponding values as choices
-                               h3("Choose values to filter for"),
-                               uiOutput("vars_prefilter"),
-                               textOutput("prefilters")),
                       
                       tabPanel("New Data Overview",
                                
                                actionButton("goDT", "Apply filters to dataset"),
-                               DT::dataTableOutput("filteredDT"))
+                               DT::dataTableOutput("filteredDT"),
+                               actionButton("goDefault", "Save default values"),
+                               tableOutput("default"),
+                               HTML("test output to see if list with default values is filled correctly"),
+                               verbatimTextOutput("lDefault"),
+                               HTML("test output to see if list with default values can be accessed correctly"),
+                               selectInput("print","choose default value to print", c("sensitivity_biomarker", "trial_struc", "sharing_type")),
+                               textOutput("printdef"))
                       
                       
                     ) 
@@ -79,38 +80,47 @@ server <- function(session, input, output){
                           sep = ";",
                           stringsAsFactors = TRUE)
   
-  exampleData <- as.data.frame(lapply(exampleData, function(y) gsub(",", ".", y)))
-  for(i in c(1:2, 5:6, 8:13)){
-    exampleData[,i] <- as.numeric(exampleData[,i])
-  }
-  for(j in c(3,4,7)){
-    exampleData[,j] <- as.factor(exampleData[,j])
-  }
+  # exampleData <- as.data.frame(lapply(exampleData, function(y) gsub(",", ".", y)))
+  # for(i in c(1:2, 5:6, 8:13)){
+  #   exampleData[,i] <- as.numeric(exampleData[,i])
+  # }
+  # for(j in c(3,4,7)){
+  #   exampleData[,j] <- as.factor(exampleData[,j])
+  # }
   
   
   # widget for user data upload
   upload <- reactive({
-    validate(need(input$file, "no file")) # if run "no file" appears everywhere upload() is called
+    validate(need(input$file, "no file")) # if no file is uploaded yet "no file" appears everywhere upload() is called
     inFile <-input$file 
     
     if(input$datatype == "Excel"){
       mydata <- read_excel(inFile$datapath,  1)
+      
     } else {
-      mydata <- read.csv(inFile$datapath, 
+      mydata <- read.csv(inFile$datapath,
+                         header = TRUE,
                          sep = input$sep, 
                          stringsAsFactors = TRUE)
-      updateCheckboxGroupInput(session, "numerics", choices = colnames(mydata))
+      # updateCheckboxGroupInput(session, "inputend", choices = colnames(mydata))
     }
     
+    
+    # for(i in 1:length(input$session)){
+    #   mydata[,input$session[i]] <- as.numeric(mydata[,input$session[i]])
+    # }    
     
     return(mydata) 
     
   })
   
   
+  
+  
   # Use example data if checkbox is checked, otherwise use uploaded dataset
   data_full <- reactive({
     if(input$exampleData){
+      updateSelectInput(session, "inputend", choices = colnames(exampleData))
       return(exampleData)
       
       # additional condition to be implemented if user changes variable classes
@@ -125,7 +135,7 @@ server <- function(session, input, output){
       #   return(upload_numerics)
       
     } else{
-      
+      updateSelectInput(session, "inputend", choices = colnames(upload()))
       return(upload())
     }
   })
@@ -150,9 +160,51 @@ server <- function(session, input, output){
   })
   
   output$filteredDT <- DT::renderDT({
-    data()
+    input$goDT
+    
+    ind_inputend <<- isolate(which(colnames(data()) == input$inputend))
+    data_filtered <<- isolate(data()[,1:ind_inputend])
+    for(i in colnames(data_filtered)){
+      data_filtered[,i] <<- as.factor(data_filtered[,i])
+    }
+    data_filtered
+    
+  }, 
+  filter = "top",
+  options = list(lengthChange = FALSE, autoWidth = TRUE)
+  )
+  
+  data_default <- reactive({
+    req(input$filteredDT_rows_all)
+    # ind_inputend <- isolate(which(colnames(data()) == input$inputend))
+    # data_filtered <<- isolate(data()[,1:ind_inputend])
+    d <- data()
+    d[input$filteredDT_rows_all, 1:ind_inputend]
   })
-
+  
+  lDefault <- eventReactive(input$goDefault,{as.list(data_default())})
+  
+  # observeEvent(input$goDefault,{
+  #   lDefault <<- as.list(data_default())
+  # })
+  
+  output$default <- renderTable({
+    input$goDefault
+    #lDefault <- as.list(data_default)
+    isolate(data_default())
+  })
+  
+  output$lDefault <- renderPrint({
+    req(lDefault)
+    print(lDefault())
+  })
+  
+  output$printdef <- renderPrint({
+    lDefault()[[input$print]]
+  })
+  
+  
+  
   # output$filteredDT <- renderDT(
   #   d
   # )
@@ -163,6 +215,5 @@ server <- function(session, input, output){
 }  
 
 shinyApp(ui = ui, server = server)
-
 
 

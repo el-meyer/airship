78eb07a8133d71b1c575de71755f7cf9f42202ae
Constantin Kumaus <constantin.kumaus@meduniwien.ac.at>
2021-08-31 17:36:31 +0200

    First functional version
    The App is functional in the following points now:
    
    - example dataset works and user can upload own dataset
    - default values for arbitrarily many input parameters can be set
    - simulation parameters can be chosen
    - lineplot on variably many dimensions (up to 4 so far) works

--- General App/General_App_DT_Filtering.R
+++ General App/General_App_DT_Filtering.R
@@ -5,74 +5,192 @@ library(readxl)
 library(xlsx)
 library(DT)
 library(shinybusy)
+library(plotly)
+library(tidyverse)
+library(shinyBS)
 
-ui <- fluidPage(title = "Simulation Results", # title in browser window tab
+ui <- 
+  fluidPage(
+    title = "Simulation Results", # title in browser window tab
+    
+    
+    # App title
+    titlePanel("Simulation Shiny App"), 
+    
+    add_busy_spinner(spin = "fading-circle"),
+    sidebarLayout(
+      
+      sidebarPanel(
+        
+        checkboxInput(
+          "exampleData", 
+          "Use example dataset"
+        ),
+        
+        
+        conditionalPanel(
+          "input.exampleData == 0",
+          
+          radioButtons(
+            "sep", 
+            "Csv-Separator", 
+            choices = c(",", ";", "\t")
+          ),
+          
+          fileInput(
+            "file", 
+            "Choose file to upload"
+          ),
+          
+          selectInput(
+            "inputend", 
+            "State last input variable", 
+            choices = NULL
+          )
+          
+          
+          
+        )
+        
+      ),
+      
+      
+      mainPanel(
+        tabsetPanel(
+          
+          tabPanel(
+            "Pre-filter data",
+            DT::dataTableOutput("dataDT")
+          ), 
+          
+          
+          tabPanel(
+            "Choose default values",
+            
+            fluidRow(
+              column(
+                12,
+                actionButton(
+                  "goDT", 
+                  "Apply filters to dataset"
+                ),
+                DT::dataTableOutput("filteredDT")
+              )
+            ),
+            
+            fluidRow(
+              column(
+                width = 6,
+                HTML("test output to see if list with default values is filled correctly"),
+                verbatimTextOutput("lDefault")
+              ),
+              
+              column(
+                width = 6,
+                actionButton(
+                  "updateDefaultList", 
+                  "Save default values"
+                ),
                 
+                checkboxInput("all_default", 
+                              "All/None", 
+                              value = TRUE
+                              ),
                 
-                # App title
-                titlePanel("Simulation Shiny App"), 
+                checkboxGroupInput(
+                  "checkboxDefault",
+                  "Which variables should have saved default values?",
+                  choices = NULL
+                )
+              )
+              
+              
+            ) 
+          ),
+          
+          tabPanel(
+            "Plot",
+            
+            fluidRow(
+              plotOutput("lineplot")
+            ),
+            
+            
+            fluidRow(
+              column(
+                6,
+                #verbatimTextOutput("test"),
                 
-                add_busy_spinner(spin = "fading-circle"),
-                sidebarLayout(
+                selectInput(
+                  "x", 
+                  "Choose x-Variable", 
+                  choices = NULL
+                ),
+                
+                checkboxGroupInput(
+                  "OC", 
+                  "Choose OC to plot", 
+                  choices = NULL
+                )
+              ),
+              
+              column(
+                6,
+                
+                checkboxInput(
+                  "checkboxFacet",
+                  "Do you want to add a facet grid dimension"
+                ),
+                
+                conditionalPanel(
+                  "input.checkboxFacet != 0",
                   
-                  
-                  sidebarPanel(
-                    
-                    checkboxInput("exampleData", "Use example dataset"),
-                    
-                    
-                    conditionalPanel("input.exampleData == 0",
-                                     
-                                     radioButtons("sep", "Csv-Separator", choices = c(",", ";", "\t")),
-                                     fileInput("file", "Choose file to upload") 
-                                     
-                                     
-                    ),
-                    
-                    selectInput("inputend", "State last input variable", choices = NULL)
-                    
-                    
+                  selectInput(
+                    "facet_rows", 
+                    "Choose row variable", 
+                    choices = NULL
                   ),
                   
+                  selectInput(
+                    "facet_cols", 
+                    "Choose col variable", 
+                    choices = NULL
+                  )
+                ),
+                
+                
+                
+                
+                checkboxInput(
+                  "checkboxShape", 
+                  "Do you want to add a shape dimension"
+                ),
+                conditionalPanel(
+                  "input.checkboxShape != 0",
                   
-                  mainPanel(
-                    tabsetPanel(
-                      
-                      tabPanel("Pre-filter data",
-                               
-                               DT::dataTableOutput("dataDT")), 
-                      
-                      
-                      tabPanel("Choose default values",
-                               
-                               fluidRow(
-                                 column(12,
-                                        
-                                        actionButton("goDT", "Apply filters to dataset"),
-                                        #actionButton("goDefault", "Save default values"),
-                                        DT::dataTableOutput("filteredDT"))),
-                               
-                               fluidRow(
-                                 column(6,
-                                        HTML("test output to see if list with default values is filled correctly"),
-                                        verbatimTextOutput("lDefault")),
-                                 
-                                 column(6,
-                                        actionButton("updateDefaultList", "Save default values"),
-                                        checkboxGroupInput("checkboxDefault",
-                                                           "Which variables should have saved default values?",
-                                                           choices = NULL)
-                                 )
-                                 
-                                 
-                               ) 
-                      )
-                      
-                    )
+                  selectInput(
+                    "shape", 
+                    "Choose shape variable",
+                    choices = NULL
                   )
                 )
-                
-)
+              )
+              
+              
+            ),
+            
+            fluidRow(
+              DT::dataTableOutput("df_plot")
+            )
+            
+          )
+        )
+      )
+      
+    )
+  )
+
+
 
 
 
@@ -85,27 +203,24 @@ server <- function(session, input, output){
   # read in Example data and convert some variables for correct display
   exampleData <- read.csv("example_data.csv", 
                           header = TRUE,
-                          sep = ";",
-                          stringsAsFactors = TRUE)
+                          sep = ",",
+                          stringsAsFactors = FALSE)
   
-  # exampleData <- as.data.frame(lapply(exampleData, function(y) gsub(",", ".", y)))
-  # for(i in c(1:2, 5:6, 8:13)){
-  #   exampleData[,i] <- as.numeric(exampleData[,i])
-  # }
-  # for(j in c(3,4,7)){
-  #   exampleData[,j] <- as.factor(exampleData[,j])
-  # }
   
   
   # widget for user data upload
   upload <- reactive({
-    validate(need(input$file, "no file")) # if no file is uploaded yet "no file" appears everywhere upload() is called
+    validate(need(
+      input$file, 
+      "no file")
+    ) # if no file is uploaded yet "no file" appears everywhere upload() is called
+    
     inFile <-input$file 
     
     mydata <- read.csv(inFile$datapath,
                        header = TRUE,
                        sep = input$sep, 
-                       stringsAsFactors = TRUE)
+                       stringsAsFactors = FALSE)
     # updateCheckboxGroupInput(session, "inputend", choices = colnames(mydata))
     
     
@@ -124,7 +239,11 @@ server <- function(session, input, output){
   # Use example data if checkbox is checked, otherwise use uploaded dataset
   data_full <- reactive({
     if(input$exampleData){
-      updateSelectInput(session, "inputend", choices = colnames(exampleData))
+      updateSelectInput(session, 
+                        "inputend", 
+                        choices = colnames(exampleData),
+                        selected = "setting"
+      )
       return(exampleData)
       
       # additional condition to be implemented if user changes variable classes
@@ -139,7 +258,10 @@ server <- function(session, input, output){
       #   return(upload_numerics)
       
     } else{
-      updateSelectInput(session, "inputend", choices = colnames(upload()))
+      updateSelectInput(session, 
+                        "inputend", 
+                        choices = colnames(upload())
+      )
       return(upload())
     }
   })
@@ -150,34 +272,87 @@ server <- function(session, input, output){
   
   
   # display dataset as DT
+  # Table in tab 'Pre-filter data'
   output$dataDT <- DT::renderDT(
     data_full(),
     filter = "top",
-    options = list(#lengthChange = FALSE, 
-      autoWidth = TRUE, 
-      scrollX = TRUE)
+    options = list(lengthChange = FALSE, 
+                   autoWidth = TRUE
+                   #, scrollX = TRUE
+    )
   )
   
-  #filter directly in DT
+  
+  # Table with all chosen filters in 'Pre-filter data'
   data <- reactive({
     req(input$dataDT_rows_all)
     d <- data_full()
+    ind_outputstart <<- isolate(which(colnames(d) == input$inputend)+1) # which column is the last input column?
+    
+    # Convert output parameters to numeric values
+    for(i in ind_outputstart:ncol(d)){
+      d[,i] <- as.numeric(d[ ,i])
+    }
+    
+    
     d[input$dataDT_rows_all,] # extracts rows that fit the filter choices
+    
   })
   
   
   
-  # Creates DT to choose default values
+  # Table displayed at start in tab 'Choose default values'
   # execute filters to create new DT upon clicking action button
   output$filteredDT <- DT::renderDT({
     input$goDT
     
-    # display only input columns
-    ind_inputend <<- isolate(which(colnames(data()) == input$inputend)) # which column is the last input column?
-    data_filtered <<- isolate(data()[,1:ind_inputend])
-    names_columns <- colnames(data()[1:ind_inputend])
+    # which column is the last input column?
+    ind_inputend <<- isolate(which(colnames(data()) == input$inputend)) 
     
-    updateCheckboxGroupInput(session, "checkboxDefault", choices = names_columns, selected = names_columns)
+    # display only input columns
+    data_filtered <<- isolate(data()[,1:ind_inputend])
+    
+    # inputvariables
+    names_inputs <<- colnames(data()[,1:ind_inputend]) 
+    
+    #outputvariables
+    names_outputs <<- colnames(data()[,(ind_inputend + 1):ncol(data())])
+    
+    
+    
+    # --------------Some choice-updates for inputs---------
+    updateCheckboxGroupInput(session, 
+                             "checkboxDefault", 
+                             choices = names_inputs, 
+                             selected = if(input$all_default) names_inputs
+    )
+    
+    updateCheckboxGroupInput(session,
+                             "OC",
+                             choices = names_outputs)
+    
+    updateSelectInput(session,
+                      "x",
+                      choices = names_inputs
+    )
+    
+    updateSelectInput(session,
+                      "facet_rows",
+                      choices = names_inputs
+    )
+    
+    
+    updateSelectInput(session,
+                      "facet_cols",
+                      choices = names_inputs
+    )
+    
+    updateSelectInput(session,
+                      "shape",
+                      choices = names_inputs
+    )
+    
+    #---------------------------------------------------------------
     
     # display only columns with more than 1 unique entry
     uniques <- lapply(data_filtered, unique)
@@ -193,51 +368,188 @@ server <- function(session, input, output){
   },
   
   filter = "top",
-  options = list(#lengthChange = FALSE, 
-    autoWidth = TRUE, 
-    scrollX = TRUE, 
-    pageLength = 5)
+  options = list(lengthChange = FALSE, 
+                 autoWidth = TRUE, 
+                 #scrollX = TRUE, 
+                 pageLength = 5)
   )
   
   
   
-  # save remaining values as defaults and put them in a list
+  # specify default value for every variable and save in data frame
+  # if df is not reduced to 1 line (i. e. if not for every variable a default value is specified), the first observation is taken as default
+  
   data_default <- reactive({
     req(input$filteredDT_rows_all)
     # ind_inputend <- isolate(which(colnames(data()) == input$inputend))
     # data_filtered <<- isolate(data()[,1:ind_inputend])
     d <- data()
-    d[input$filteredDT_rows_all, 1:ind_inputend]
+    d <- d[input$filteredDT_rows_all, 1:ind_inputend]
+    d[1,]
   })
   
-  #lDefault <- eventReactive(input$goDefault,{as.list(data_default())})
   
-  lDefault <- eventReactive(input$updateDefaultList, {as.list(data_default()[input$checkboxDefault])})
   
-  # observeEvent(input$goDefault,{
-  #   lDefault <<- as.list(data_default())
-  # })
   
-  output$default <- renderTable({
-    input$goDefault
-    #lDefault <- as.list(data_default)
-    isolate(data_default())
-  })
   
+  
+
+  # save default values in a list upon clicking action button
+  lDefault <- eventReactive(input$updateDefaultList, {as.list(data_default()[1, input$checkboxDefault])})
+  
+  
+  # Output of list with default values
   output$lDefault <- renderPrint({
     req(lDefault)
     print(lDefault())
   })
   
+
   
   
   
   
+  # PLOT -------------------------------------------------------------------
+
+  # Data frame used for plot
+  # Filters every variable for the specified default value except the chosen simulation parameters, which can have more distinguishable values
+  
+  df_plot <- reactive({
+    
+    # 1 line df with default values for variables that are checked
+    default_df <- data_default()[1, input$checkboxDefault]
+    
+    # vector of names of simulation parameters
+    sim_par <- input$x
+    if(input$checkboxShape){
+      sim_par <- c(sim_par, input$shape)
+    }
+    
+    if(input$checkboxFacet){
+      sim_par <- c(sim_par, input$facet_rows, input$facet_cols)
+    }
+    
+    # exclude simulation parameters from df with default values
+    default_filter <- default_df[!(names(default_df) %in% sim_par)]
+    
+    bedingung <- paste0(names(default_filter),
+                        " == ",
+                        paste0("'", default_filter[1,], "'"),
+                        collapse = " & ")
+    
+    
+    df_plot <- subset(data(), eval(parse(text = bedingung)))
+    df_plot # return data frame
+  })
+  
+  
+  
+  output$df_plot <- DT::renderDT({
+    
+    df_plot()
+  })
+  
+  
+  
+  # Transform dataset to long format on chosen output variables for easy plotting
+  data_longer <- reactive({
+    
+    
+    d <- df_plot()
+    # d <- d[input$filteredDT_rows_all,]
+    
+    d <- 
+      d %>%
+      pivot_longer(cols = input$OC,
+                   names_to = "OC",
+                   values_to = "value")
+    d
+  })
+  
+  
+  
+  # Plot based on which dimensions are chosen
+  output$lineplot<- renderPlot({
+    p1 <- ggplot(
+      data_longer(), 
+      aes_string(x = input$x)
+    )
+
+    p1 <- 
+      p1 + 
+      geom_line(aes(y = value, color = OC)) +
+      geom_point(aes(y = value, color = OC))
+    
+    
+    
+    if(input$checkboxShape){
+      p1 <- 
+        p1 + 
+        aes(linetype = factor(get(input$shape)),
+            shape = factor(get(input$shape)))
+    }
+    
+
+    if(input$checkboxFacet){
+      p1 <- 
+        p1 + 
+        facet_grid(vars(get(input$facet_rows)),
+                   vars(get(input$facet_cols))
+                   )
+    }
+    
+    p1
+    
+  })
+  
+  
+ 
 }  
 
 shinyApp(ui = ui, server = server)
 
+#shinyBS::bsModal()
 
 
+
+
+
+
+
+# data_test <- read.csv("example_data.csv",
+#                       header = TRUE,
+#                       sep = ",",
+#                       stringsAsFactors = FALSE)
+# 
+# default_list <- as.list(data_test[1,1:which(names(data_test) == "setting")])
+# 
+# 
+# # default_df <- as.data.frame(do.call(cbind, default_list))
+# 
+# default_df <- as.data.frame(default_list)
+# 
+# 
+# 
+# sim_par <- c("n_int", "sharing_type", "cohorts_max", "sensitivity_biomarker")
+# default_filter <- default_df[!(names(default_df) %in% sim_par)]
+# 
+# bedingung <- paste0(names(default_filter),
+#                     " == ",
+#                     paste0("'", default_filter[1,], "'"),
+#                     collapse = " & ")
+# 
+# 
+# 
+# df_plot <- subset(data_test, eval(parse(text = bedingung)))
+# dim(df_plot)
+# 
+# remove.factors(df_plot)
+# 
+# p <- ggplot(df_plot, aes(x = n_int))
+# p <- p + geom_line(aes(y = FWER, color = factor(sensitivity_biomarker), linetype = factor(sensitivity_biomarker)))
+# p <- p + facet_grid(rows = vars(sharing_type), cols = vars(cohorts_max))
+# p
+# 
+# 
 
 

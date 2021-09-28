fd0287d8b4936bd22143a6798cacca115cef7057
Constantin Kumaus <constantin.kumaus@meduniwien.ac.at>
2021-09-28 14:54:35 +0200

    Bug fixes and color specification
    Now the user can define the colour of each output when it is plotted

--- General App/General_App_DT_Filtering_28_09.R
+++ General App/General_App_DT_Filtering_28_09.R
@@ -1,14 +1,4 @@
-options(shiny.sanitize.errors = FALSE) 
-options(shiny.maxRequestSize = 50*1024^2)
-library(shiny)
-library(readxl)
-library(xlsx)
-library(DT)
-library(shinybusy)
-library(plotly)
-library(tidyverse)
-library(shinyBS)
-library(colourpicker)
+
 
 ui <- 
   fluidPage(
@@ -75,6 +65,7 @@ ui <-
                   "goDT", 
                   "Apply filters to dataset"
                 ),
+                verbatimTextOutput("colors"),
                 verbatimTextOutput("search"),
                 verbatimTextOutput("searchInput"),
                 DT::dataTableOutput("filteredDT")
@@ -94,6 +85,10 @@ ui <-
                 actionButton(
                   "updateDefaultList", 
                   "Save default values"
+                ),
+                actionButton(
+                  "updateDefaultList2",
+                  "Start Color Picker"
                 )
                 # ,
                 # 
@@ -114,11 +109,18 @@ ui <-
           ),
           
           tabPanel(
+            "Colors",
+            
+            uiOutput("colors_ui")
+            , verbatimTextOutput("colorText")
+          ),
+          
+          tabPanel(
             "Plot",
             
             fluidRow(
               uiOutput("lineplot_ui")
-              # plotOutput("lineplot")
+              #, plotOutput("lineplot")
             ),
             
             
@@ -132,6 +134,9 @@ ui <-
                   "Choose x-Variable", 
                   choices = NULL
                 ),
+                
+                # colourPicker(3),
+                
                 
                 checkboxGroupInput(
                   "OC", 
@@ -184,11 +189,28 @@ ui <-
               
               column(
                 4,
-                sliderInput("res",
-                            "Change resolution",
-                            value = 72,
-                            min = 50, 
-                            max = 200),
+                
+                sliderInput("xLegend",
+                            "x-coord legend",
+                            min = -0.5,
+                            max = 1.5,
+                            value = 0, 
+                            step = 0.05),
+                
+                
+                
+                sliderInput("yLegend",
+                            "y-coord legend",
+                            min = -0.5,
+                            max = 1.5,
+                            value = 0,
+                            step = 0.05),
+                # 
+                #                 sliderInput("res",
+                #                             "Change resolution",
+                #                             value = 72,
+                #                             min = 50, 
+                #                             max = 200),
                 
                 checkboxInput(
                   "checkboxSize", 
@@ -245,11 +267,20 @@ ui <-
                     step = 1
                   ),
                   
+                  # colorPickr(
+                  #   inputId = "plot_title_colour", label = "Title colour:",
+                  #   #showColour = "background",
+                  #   selected = "black",
+                  #   opacity = FALSE)
+                  # 
                   colourInput(
                     inputId = "plot_title_colour", label = "Title colour:",
                     showColour = "background",
                     value = "black",
                     allowTransparent = FALSE)
+                  
+                  
+                  
                   
                   
                   
@@ -398,6 +429,18 @@ server <- function(session, input, output){
     )
   )
   
+  reacVals <- reactiveValues()
+  
+  observe({
+    reacVals$ind_inputend <- which(colnames(data_full()) == input$inputend)
+    #reacVals$ind_outputstart <- which(colnames(data_full() == input$inputend) +1)
+  })
+  
+  observe({
+    reacVals$ind_outputstart <- reacVals$ind_inputend + 1
+  })
+  
+  
   
   # Table with all chosen filters in 'Pre-filter data'
   data_prefiltered <- reactive({
@@ -426,16 +469,16 @@ server <- function(session, input, output){
     
     
     # which column is the last input column?
-    ind_inputend <<- isolate(which(colnames(data_prefiltered()) == input$inputend)) 
+    ind_inputend <- isolate(which(colnames(data_prefiltered()) == input$inputend)) 
     
     # display only input columns
-    data_filtered <<- isolate(data_prefiltered()[,1:ind_inputend])
+    data_filtered <<- data_prefiltered()[,1:ind_inputend]
     
     # inputvariables
-    names_inputs <<- colnames(data_prefiltered()[,1:ind_inputend]) 
+    names_inputs <- colnames(data_prefiltered()[,1:ind_inputend])
     
     #outputvariables
-    names_outputs <<- colnames(data_prefiltered()[,(ind_inputend + 1):ncol(data_prefiltered())])
+    names_outputs <- colnames(data_prefiltered()[, (ind_inputend + 1):ncol(data_prefiltered())])
     
     
     
@@ -577,7 +620,105 @@ server <- function(session, input, output){
     
   })
   
-  # --------------------------------------------------------------------
+  # Color vector specification ------------------------------------
+  
+  
+  
+  # Adding reactive Values for number of OCs and names of OCs
+  
+  observe({
+    
+    reacVals$nOC <- ncol(data_prefiltered()) - reacVals$ind_inputend
+    
+  })
+  
+  observe({
+    
+    reacVals$names_outputs <- colnames(data_prefiltered()[,reacVals$ind_outputstart:ncol(data_prefiltered())])
+  })
+  
+  
+  # experimenting with color() palette for plot --------------------------------
+  
+  
+  # vColors <- reactive({
+  #   
+  #   # ind_inputend <- isolate(which(colnames(data_prefiltered()) == input$inputend))
+  #   names_outputs <- colnames(data_prefiltered()[,reacVals$ind_outputstart:ncol(data_prefiltered())])
+  #   
+  #   
+  #   nOC <- ncol(data_prefiltered()) - reacVals$ind_inputend
+  #   
+  #   nColors <- sample(1:657,
+  #                     size = nOC,
+  #                     replace = FALSE)
+  #   vColors <- colors()[nColors]
+  #   names(vColors) <- factor(factor(names_outputs))
+  #   
+  #   # output <- c(nOC, reacVals$ind_inputend, input$inputend, reacVals$ind_outputstart)
+  #   # names(output) <- c("nOC", "ind_inputend", "input$inputend", "ind_outputstart")
+  #   # output
+  #   vColors
+  #   
+  # })
+  # 
+  # output$colors <- renderPrint({
+  #   
+  #   vColors()
+  # })
+  
+  
+  
+  # --------------------------------------------------------------------------
+  # dynamic number of color selectors (one for every OC) ---------------------
+  
+  
+  output$colors_ui <- renderUI({
+    
+    nWidgets <- as.integer(reacVals$nOC)
+    
+    lapply(1:nWidgets, function(i) {
+      colourInput(inputId = paste0("col", i), 
+                  label = reacVals$names_outputs[i],
+                  showColour = "background",
+                  # value = "black"
+                  value = colors()[sample(1:657,
+                                          size = 1,
+                                          replace = FALSE)]
+                  )
+    })
+    
+    
+  })
+  
+  lUiColors <- reactive({
+    
+    nWidgets <- as.integer(reacVals$nOC)
+    #names_outputs <- colnames(data_prefiltered()[,reacVals$ind_outputstart:ncol(data_prefiltered())])
+    
+    df_colors <- data.frame(lapply(1:nWidgets, function(i) {
+      input[[paste0("col", i)]]
+    }))
+    
+    names(df_colors) <- reacVals$names_outputs
+    
+    df_colors
+    
+  })
+  
+
+  output$colorText <- renderPrint({
+   lUiColors()
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  # --------------------------------------------------------------------------
   # only make variables with default values available for simulation parameter choice
   
   observe({
@@ -646,7 +787,9 @@ server <- function(session, input, output){
   # 
   
   # variant with automatic input of chosen filters as default values
-  lDefault <- eventReactive(input$updateDefaultList, {as.list(search_input())})
+  lDefault <- eventReactive(input$updateDefaultList, 
+                            {as.list(search_input())})
+  
   
   
   
@@ -746,10 +889,13 @@ server <- function(session, input, output){
       aes_string(x = input$x)
     )
     
+    colScale <- scale_colour_manual(values = lUiColors())
+    
     p1 <- 
       p1 + 
       geom_line(aes(y = value, color = OC), size = input$linesize) +
-      geom_point(aes(y = value, color = OC), size = 3*input$linesize)
+      geom_point(aes(y = value, color = OC), size = 3*input$linesize) +
+      colScale
     
     
     
@@ -758,13 +904,13 @@ server <- function(session, input, output){
         p1 + 
         aes(linetype = 
               factor(get(
-                input$shape
-              ))
+                input$shape)
+              )
             ,
             shape = 
               factor(get(
-                input$shape
-              ))
+                input$shape)
+              )
         )
     }
     
@@ -817,7 +963,10 @@ server <- function(session, input, output){
       
     }
     
-    ggplotly(p1)
+    p1 <- p1 + theme(legend.title = element_blank())
+    p1 <- ggplotly(p1)
+    
+    p1 %>% layout(legend = list(x = input$xLegend, y = input$yLegend))
     
     
     
@@ -825,8 +974,8 @@ server <- function(session, input, output){
   
   output$lineplot_ui <- renderUI({
     plotlyOutput("lineplot",
-              height = input$plotheight,
-              width = input$plotwidth)
+                 height = input$plotheight,
+                 width = input$plotwidth)
   })
   
   

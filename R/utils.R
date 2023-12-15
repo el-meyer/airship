616285e46aa7a2cdff86cc6b1154f441c789ba16
Michal Majka <michalmajka@hotmail.com>
2023-12-15 22:03:05 +0100

    Closes #145

---
+++ R/utils.R
@@ -0,0 +1,23 @@
+.onAttach <- function(libname, pkgname) {
+  
+  v <- getNamespaceVersion("airship")
+  rule <- paste0(rep("-", getOption("width")), collapse = "")
+  
+  packageStartupMessage(rule)
+  packageStartupMessage(paste0("airship ", v, " loaded."))
+  packageStartupMessage("Visit for more information: ")
+  packageStartupMessage("https://el-meyer.github.io/airship/")
+  packageStartupMessage(rule)
+  
+}
+
+
+.onDetach <- function(libpath) {
+  
+  rule <- paste0(rep("-", getOption("width")), collapse = "")
+  packageStartupMessage(rule)
+  packageStartupMessage("Thank you for using the airship package!")
+  packageStartupMessage("Don't forget to report bugs / request features under:")
+  packageStartupMessage("https://github.com/el-meyer/airship/issues")
+  packageStartupMessage(rule)
+}
